390506f71adc64f56e9ce9ee4aadd2edd5b698e5
alanRobert <pkggame30@gmail.com>
2024-10-24 15:34:42 +0700

    new file-php

---
+++ index3.php
@@ -0,0 +1,19 @@
+<?php
+
+$nilaiAwal = 0;
+$nilaiAkhir = 10;
+
+// while
+echo 'menggunakan While<br />';
+while($nilaiAwal < $nilaiAkhir) {
+        echo 'Hello World';
+        echo '<br />';
+        $nilaiAwal++;
+}
+
+// for
+echo '<br />menggunakan For<br />';
+for($i = 0; $i < 10; $i++) {
+        echo 'Hello World';
+        echo '<br />';
+}
